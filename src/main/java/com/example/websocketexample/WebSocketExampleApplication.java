f474325238ebfbf92a3a6bc0d52fb85ac4d7337c
Angelina Tsaryova <carevaad@vtb.ru>
2025-07-25 11:31:50 +0300

    WebSocket example

---
+++ src/main/java/com/example/websocketexample/WebSocketExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocketexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketExampleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/websocketexample/model/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.example.websocketexample.model;
+
+import lombok.Data;
+
+@Data
+public class ChatMessage {
+    private MessageType type;
+    private String content;
+    private String sender;
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
